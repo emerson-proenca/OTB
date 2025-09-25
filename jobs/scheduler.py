56aa244d396dd6f32da4b52e5bd3ec4b3c45ea32
Emerson <79228789+Emersh0w@users.noreply.github.com>
2025-09-25 18:46:14 -0300

    trocar print por logger.info no scheduler.py

--- jobs/scheduler.py
+++ jobs/scheduler.py
@@ -1,14 +1,16 @@
 # jobs/scheduler.py
+import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from jobs.sync_tournaments import sync_cbxtournaments
-import time
+from core.logger_config import logger
+
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
     # roda todo dia (24h), aqui definido por segundos apenas para exemplo:
     scheduler.add_job(sync_cbxtournaments, "interval", hours=24, id="sync_cbx")
     scheduler.start()
-    print("Scheduler started")
+    logger.info(f"Scheduler started")
     try:
         while True:
             time.sleep(60)
